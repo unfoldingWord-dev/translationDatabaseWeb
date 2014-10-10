93bfef9bed1ae1353a3c103f1d5d547eed5fc584
Patrick Altman <paltman@gmail.com>
2014-10-10 12:50:09 -0500

    Add verbose name for the main app
    This enables better header display in the admin

--- td/apps.py
+++ td/apps.py
@@ -5,6 +5,7 @@ from django.utils.importlib import import_module
 class AppConfig(BaseAppConfig):
 
     name = "td"
+    verbose_name = "Translation Database"
 
     def ready(self):
         import_module("td.receivers")
