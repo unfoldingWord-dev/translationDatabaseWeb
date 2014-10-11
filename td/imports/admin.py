a911488a06977f2606f2f7d7094977a7f5a15daf
Patrick Altman <paltman@gmail.com>
2014-10-11 01:43:33 -0500

    Lock down admin for imports
    We need to ensure that imported data is never changed, updated, or
    deleted. We will link to this data, copy it into other tables, etc.,
    to merge with our own created data.

--- td/imports/admin.py
+++ td/imports/admin.py
@@ -9,51 +9,43 @@ from .models import (
 )
 
 
-admin.site.register(
-    EthnologueCountryCode,
-    list_display=[
-        "code", "name", "area", "date_imported"
-    ],
-    list_filter=[
-        "area"
-    ],
-    search_fields=[
-        "code", "name", "area"
-    ]
-)
+class LockedDownModelAdmin(admin.ModelAdmin):
+
+    actions = None
+
+    def has_add_permission(self, request):
+        return False
+
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+    def get_readonly_fields(self, request, obj=None):
+        return [f.name for f in self.model._meta.fields]
+
+    def save_model(self, request, obj, form, change):
+        pass
 
 
-admin.site.register(
-    EthnologueLanguageCode,
-    list_display=[
-        "code", "country_code", "status", "name", "date_imported"
-    ],
-    list_filter=[
-        "country_code", "status"
-    ],
-    search_fields=[
-        "code", "name"
-    ]
-)
+class EthnologueCountryCodeAdmin(LockedDownModelAdmin):
+    list_display = ["code", "name", "area", "date_imported"]
+    list_filter = ["area"]
+    search_fields = ["code", "name", "area"]
 
 
-admin.site.register(
-    EthnologueLanguageIndex,
-    list_display=[
-        "language_code", "country_code", "name_type", "name", "date_imported"
-    ],
-    list_filter=[
-        "name_type", "country_code"
-    ],
-    search_fields=[
-        "language_code", "name"
-    ]
-)
+class EthnologueLanguageCodeAdmin(LockedDownModelAdmin):
+    list_display = ["code", "country_code", "status", "name", "date_imported"]
+    list_filter = ["country_code", "status"]
+    search_fields = ["code", "name"]
 
 
-admin.site.register(
-    WikipediaISOLanguage,
-    list_display=[
+class EthnologueLanguageIndexAdmin(LockedDownModelAdmin):
+    list_display = ["language_code", "country_code", "name_type", "name", "date_imported"]
+    list_filter = ["name_type", "country_code"]
+    search_fields = ["language_code", "name"]
+
+
+class WikipediaISOLanguageAdmin(LockedDownModelAdmin):
+    list_display = [
         "language_family",
         "language_name",
         "native_name",
@@ -64,21 +56,13 @@ admin.site.register(
         "iso_639_9",
         "notes",
         "date_imported"
-    ],
-    list_filter=[
-        "language_family"
-    ],
-    search_fields=[
-        "language_name",
-        "native_name",
-        "notes"
     ]
-)
+    list_filter = ["language_family"]
+    search_fields = ["language_name", "native_name", "notes"]
 
 
-admin.site.register(
-    SIL_ISO_639_3,
-    list_display=[
+class SIL_ISO_639_3Admin(LockedDownModelAdmin):
+    list_display = [
         "code",
         "part_2b",
         "part_2t",
@@ -88,13 +72,13 @@ admin.site.register(
         "ref_name",
         "comment",
         "date_imported",
-    ],
-    list_filter=[
-        "scope",
-        "language_type"
-    ],
-    search_fields=[
-        "comment",
-        "ref_name"
     ]
-)
+    list_filter = ["scope", "language_type"]
+    search_fields = ["comment", "ref_name"]
+
+
+admin.site.register(EthnologueCountryCode, EthnologueCountryCodeAdmin)
+admin.site.register(EthnologueLanguageCode, EthnologueLanguageCodeAdmin)
+admin.site.register(EthnologueLanguageIndex, EthnologueLanguageIndexAdmin)
+admin.site.register(WikipediaISOLanguage, WikipediaISOLanguageAdmin)
+admin.site.register(SIL_ISO_639_3, SIL_ISO_639_3Admin)
