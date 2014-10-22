7cabc38c0c65cb10cec8c8f7ea6ca5200dd37fb1
Patrick Altman <paltman@gmail.com>
2014-10-21 19:03:58 -0500

    Expose imported data via public tables

--- td/urls.py
+++ td/urls.py
@@ -5,6 +5,14 @@ from django.views.generic import TemplateView
 
 from django.contrib import admin
 
+from .views import (
+    AdditionalLanguageListView,
+    EthnologueCountryCodeListView,
+    EthnologueLanguageCodeListView,
+    EthnologueLanguageIndexListView,
+    SIL_ISO_639_3ListView,
+    WikipediaISOLanguageListView
+)
 
 urlpatterns = patterns(
     "",
@@ -15,6 +23,13 @@ urlpatterns = patterns(
     url(r"^exports/codes-d43.txt$", "td.views.codes_text_export", name="codes_text_export"),
     url(r"^exports/langnames.txt$", "td.views.names_text_export", name="names_text_export"),
     url(r"^exports/langnames.json$", "td.views.names_json_export", name="names_json_export"),
+
+    url(r"^data-sources/additional-languages/$", AdditionalLanguageListView.as_view(), name="ds_additional_languages"),
+    url(r"^data-sources/ethnologue/country-codes/$", EthnologueCountryCodeListView.as_view(), name="ds_ethnologue_country_codes"),
+    url(r"^data-sources/ethnologue/language-codes/$", EthnologueLanguageCodeListView.as_view(), name="ds_ethnologue_language_codes"),
+    url(r"^data-sources/ethnologue/language-index/$", EthnologueLanguageIndexListView.as_view(), name="ds_ethnologue_language_index"),
+    url(r"^data-sources/sil-iso-639-3/$", SIL_ISO_639_3ListView.as_view(), name="ds_sil"),
+    url(r"^data-sources/wikipedia/$", WikipediaISOLanguageListView.as_view(), name="ds_wikipedia"),
 )
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- td/views.py
+++ td/views.py
@@ -1,6 +1,16 @@
 from django.http import HttpResponse
+from django.views.generic import ListView
+
+from td.imports.models import (
+    EthnologueCountryCode,
+    EthnologueLanguageCode,
+    EthnologueLanguageIndex,
+    SIL_ISO_639_3,
+    WikipediaISOLanguage
+)
 
 from .exports import LanguageCodesExport, LanguageNamesExport
+from .models import AdditionalLanguage
 
 
 def codes_text_export(request):
@@ -13,3 +23,37 @@ def names_text_export(reqeust):
 
 def names_json_export(request):
     return HttpResponse(LanguageNamesExport().json, content_type="application/json")
+
+
+class AdditionalLanguageListView(ListView):
+    model = AdditionalLanguage
+    paginate_by = 50
+
+
+class EthnologueCountryCodeListView(ListView):
+    model = EthnologueCountryCode
+    template_name = "td/ethnologuecountrycode_list.html"
+    paginate_by = 50
+
+
+class EthnologueLanguageCodeListView(ListView):
+    model = EthnologueLanguageCode
+    template_name = "td/ethnologuelanguagecode_list.html"
+    paginate_by = 50
+
+
+class EthnologueLanguageIndexListView(ListView):
+    model = EthnologueLanguageIndex
+    template_name = "td/ethnologuelanguageindex_list.html"
+    paginate_by = 50
+
+
+class SIL_ISO_639_3ListView(ListView):
+    model = SIL_ISO_639_3
+    template_name = "td/sil_list.html"
+    paginate_by = 50
+
+
+class WikipediaISOLanguageListView(ListView):
+    model = WikipediaISOLanguage
+    template_name = "td/wikipedia_list.html"
