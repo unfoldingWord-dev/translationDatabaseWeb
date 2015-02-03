c1b6b3604af6b2253220be330c0d1410f75807bd
Patrick Altman <paltman@gmail.com>
2015-02-02 20:33:34 -0600

    Add celery bootstrapping

--- td/apps.py
+++ td/apps.py
@@ -1,5 +1,10 @@
+from __future__ import absolute_import
+
 from django.apps import AppConfig as BaseAppConfig
+from django.conf import settings
 from django.utils.importlib import import_module
+
+from celery import Celery
 
 
 class AppConfig(BaseAppConfig):
@@ -8,4 +13,7 @@ class AppConfig(BaseAppConfig):
     verbose_name = "Translation Database"
 
     def ready(self):
+        app = Celery("td")
+        app.config_from_object("django.conf:settings")
+        app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
         import_module("td.receivers")
