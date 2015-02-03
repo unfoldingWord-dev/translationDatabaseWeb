615fc076895a7a6e116d7a551c025d4bcfd23c81
Patrick Altman <paltman@gmail.com>
2015-02-02 21:00:11 -0600

    Try something diff with celery config

--- td/__init__.py
+++ td/__init__.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+from __future__ import absolute_import
 
 __about__ = """
 In addition to what is provided by the "zero" project, this project
@@ -8,3 +9,7 @@ suitable for most sites that have user accounts.
 """
 
 default_app_config = "td.apps.AppConfig"
+
+# This will make sure the app is always imported when
+# Django starts so that shared_task will use this app.
+from .celery import app as celery_app  # noqa
