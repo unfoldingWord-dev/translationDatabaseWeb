5bfabf6b2cb16b421177bb795d53881a75864346
Patrick Altman <paltman@gmail.com>
2015-02-07 15:23:43 -0600

    Fix syntax error

--- td/tests/test_models.py
+++ td/tests/test_models.py
@@ -39,7 +39,7 @@ class LanguageIntegrationTests(TestCase):
         ethno = open(os.path.join(os.path.dirname(__file__), "../imports/tests/data/LanguageCodes.tab")).read()  # noqa
         country = open(os.path.join(os.path.dirname(__file__), "../imports/tests/data/CountryCodes.tab")).read()  # noqa
         sil = open(os.path.join(os.path.dirname(__file__), "../imports/tests/data/iso_639_3.tab")).read()  # noqa
-        with patch.object("requests.Session") as mock_requests:
+        with patch("requests.Session") as mock_requests:
             # mock_requests.return_value = mock_response = Mock()
             mock_requests.get().status_code = 200
             mock_requests.get().content = wikipedia
