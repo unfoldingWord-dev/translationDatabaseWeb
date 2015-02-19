c3ae6fc923dde2411240bd225d1c8ce15e14fa41
swilcox <steven@wilcoxzone.com>
2015-02-18 21:16:38 -0600

    added new region seed and redid uw migration

--- td/uw/migrations/0001_initial.py
+++ td/uw/migrations/0001_initial.py
@@ -2,11 +2,13 @@
 from __future__ import unicode_literals
 
 from django.db import models, migrations
+import django.utils.timezone
 
 
 class Migration(migrations.Migration):
 
     dependencies = [
+        ('contenttypes', '0001_initial'),
     ]
 
     operations = [
@@ -21,15 +23,46 @@ class Migration(migrations.Migration):
             bases=(models.Model,),
         ),
         migrations.CreateModel(
+            name='BibleContentEAV',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('attribute', models.CharField(max_length=100)),
+                ('value', models.CharField(max_length=250)),
+                ('source_id', models.IntegerField()),
+                ('timestamp', models.DateTimeField(default=django.utils.timezone.now)),
+                ('entity', models.ForeignKey(related_name='attributes', to='uw.BibleContent')),
+                ('source_ct', models.ForeignKey(to='contenttypes.ContentType')),
+            ],
+            options={
+                'abstract': False,
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
             name='Country',
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
                 ('code', models.CharField(unique=True, max_length=2)),
                 ('name', models.CharField(max_length=75)),
-                ('area', models.CharField(max_length=10)),
                 ('population', models.IntegerField(null=True, blank=True)),
             ],
             options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='CountryEAV',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('attribute', models.CharField(max_length=100)),
+                ('value', models.CharField(max_length=250)),
+                ('source_id', models.IntegerField()),
+                ('timestamp', models.DateTimeField(default=django.utils.timezone.now)),
+                ('entity', models.ForeignKey(related_name='attributes', to='uw.Country')),
+                ('source_ct', models.ForeignKey(to='contenttypes.ContentType')),
+            ],
+            options={
+                'abstract': False,
             },
             bases=(models.Model,),
         ),
@@ -48,12 +81,44 @@ class Migration(migrations.Migration):
             bases=(models.Model,),
         ),
         migrations.CreateModel(
+            name='LanguageEAV',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('attribute', models.CharField(max_length=100)),
+                ('value', models.CharField(max_length=250)),
+                ('source_id', models.IntegerField()),
+                ('timestamp', models.DateTimeField(default=django.utils.timezone.now)),
+                ('entity', models.ForeignKey(related_name='attributes', to='uw.Language')),
+                ('source_ct', models.ForeignKey(to='contenttypes.ContentType')),
+            ],
+            options={
+                'abstract': False,
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
             name='Network',
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
                 ('name', models.CharField(max_length=100)),
             ],
             options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='NetworkEAV',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('attribute', models.CharField(max_length=100)),
+                ('value', models.CharField(max_length=250)),
+                ('source_id', models.IntegerField()),
+                ('timestamp', models.DateTimeField(default=django.utils.timezone.now)),
+                ('entity', models.ForeignKey(related_name='attributes', to='uw.Network')),
+                ('source_ct', models.ForeignKey(to='contenttypes.ContentType')),
+            ],
+            options={
+                'abstract': False,
             },
             bases=(models.Model,),
         ),
@@ -73,6 +138,34 @@ class Migration(migrations.Migration):
             bases=(models.Model,),
         ),
         migrations.CreateModel(
+            name='OrganizationEAV',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('attribute', models.CharField(max_length=100)),
+                ('value', models.CharField(max_length=250)),
+                ('source_id', models.IntegerField()),
+                ('timestamp', models.DateTimeField(default=django.utils.timezone.now)),
+                ('entity', models.ForeignKey(related_name='attributes', to='uw.Organization')),
+                ('source_ct', models.ForeignKey(to='contenttypes.ContentType')),
+            ],
+            options={
+                'abstract': False,
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='Region',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.CharField(max_length=100, db_index=True)),
+                ('slug', models.SlugField(max_length=100)),
+            ],
+            options={
+                'ordering': ['name'],
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
             name='Resource',
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
@@ -83,6 +176,22 @@ class Migration(migrations.Migration):
                 ('language', models.ForeignKey(to='uw.Language')),
             ],
             options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='ResourceEAV',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('attribute', models.CharField(max_length=100)),
+                ('value', models.CharField(max_length=250)),
+                ('source_id', models.IntegerField()),
+                ('timestamp', models.DateTimeField(default=django.utils.timezone.now)),
+                ('entity', models.ForeignKey(related_name='attributes', to='uw.Resource')),
+                ('source_ct', models.ForeignKey(to='contenttypes.ContentType')),
+            ],
+            options={
+                'abstract': False,
             },
             bases=(models.Model,),
         ),
@@ -102,6 +211,22 @@ class Migration(migrations.Migration):
             bases=(models.Model,),
         ),
         migrations.CreateModel(
+            name='ScriptureEAV',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('attribute', models.CharField(max_length=100)),
+                ('value', models.CharField(max_length=250)),
+                ('source_id', models.IntegerField()),
+                ('timestamp', models.DateTimeField(default=django.utils.timezone.now)),
+                ('entity', models.ForeignKey(related_name='attributes', to='uw.Scripture')),
+                ('source_ct', models.ForeignKey(to='contenttypes.ContentType')),
+            ],
+            options={
+                'abstract': False,
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
             name='TranslationNeed',
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
@@ -112,6 +237,22 @@ class Migration(migrations.Migration):
                 ('language', models.ForeignKey(to='uw.Language')),
             ],
             options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='TranslationNeedEAV',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('attribute', models.CharField(max_length=100)),
+                ('value', models.CharField(max_length=250)),
+                ('source_id', models.IntegerField()),
+                ('timestamp', models.DateTimeField(default=django.utils.timezone.now)),
+                ('entity', models.ForeignKey(related_name='attributes', to='uw.TranslationNeed')),
+                ('source_ct', models.ForeignKey(to='contenttypes.ContentType')),
+            ],
+            options={
+                'abstract': False,
             },
             bases=(models.Model,),
         ),
@@ -134,6 +275,22 @@ class Migration(migrations.Migration):
             bases=(models.Model,),
         ),
         migrations.CreateModel(
+            name='TranslatorEAV',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('attribute', models.CharField(max_length=100)),
+                ('value', models.CharField(max_length=250)),
+                ('source_id', models.IntegerField()),
+                ('timestamp', models.DateTimeField(default=django.utils.timezone.now)),
+                ('entity', models.ForeignKey(related_name='attributes', to='uw.Translator')),
+                ('source_ct', models.ForeignKey(to='contenttypes.ContentType')),
+            ],
+            options={
+                'abstract': False,
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
             name='WorkInProgress',
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
@@ -143,6 +300,22 @@ class Migration(migrations.Migration):
                 ('bible_content', models.ForeignKey(verbose_name=b'Bible Content', to='uw.BibleContent')),
                 ('language', models.ForeignKey(to='uw.Language')),
                 ('translators', models.ManyToManyField(to='uw.Translator', blank=True)),
+            ],
+            options={
+                'abstract': False,
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='WorkInProgressEAV',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('attribute', models.CharField(max_length=100)),
+                ('value', models.CharField(max_length=250)),
+                ('source_id', models.IntegerField()),
+                ('timestamp', models.DateTimeField(default=django.utils.timezone.now)),
+                ('entity', models.ForeignKey(related_name='attributes', to='uw.WorkInProgress')),
+                ('source_ct', models.ForeignKey(to='contenttypes.ContentType')),
             ],
             options={
                 'abstract': False,
@@ -165,6 +338,12 @@ class Migration(migrations.Migration):
             model_name='country',
             name='primary_networks',
             field=models.ManyToManyField(to='uw.Network', blank=True),
+            preserve_default=True,
+        ),
+        migrations.AddField(
+            model_name='country',
+            name='region',
+            field=models.ForeignKey(related_name='countries', blank=True, to='uw.Region', null=True),
             preserve_default=True,
         ),
     ]
