13ae2cc361259c110590d2894567b3dce78a6bc1
swilcox <steven@wilcoxzone.com>
2015-02-23 20:19:07 -0600

    slight tweak to imb integration

--- td/tasks.py
+++ td/tasks.py
@@ -1,7 +1,6 @@
 from __future__ import absolute_import
 
 from django.db import connection
-from django.core.exceptions import ObjectDoesNotExist
 from celery import task
 from eventlog.models import log
 
