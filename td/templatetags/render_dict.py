51d526e89d86a525de8660216f3c192e7b5870ee
swilcox <steven@wilcoxzone.com>
2015-03-18 22:23:51 -0500

    display resource 'extra_data' field as a horizontal description list

---
+++ td/templatetags/render_dict.py
@@ -0,0 +1,17 @@
+from django import template
+from django.utils.safestring import mark_safe
+
+
+register = template.Library()
+
+
+@register.filter
+def dict_to_dl(dictvalue, css_class="dl-horizontal"):
+    result = []
+    for key, val in dictvalue.iteritems():
+        result.append("<dt>{0}</dt><dd>{1}</dd>".format(key, val))
+    if len(result):
+        result.insert(0, '<dl class="{0}">'.format(css_class))
+        result.append("</dl>")
+    result_str = "".join(result)
+    return mark_safe(result_str)
