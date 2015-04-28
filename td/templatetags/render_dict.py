e7bd9d9e391ceadf1f3b8cc4ecd0324a2199554a
Patrick Altman <paltman@gmail.com>
2015-04-28 14:10:50 -0500

    Safely handle non-dict values

--- td/templatetags/render_dict.py
+++ td/templatetags/render_dict.py
@@ -8,8 +8,9 @@ register = template.Library()
 @register.filter
 def dict_to_dl(dictvalue, css_class="dl-horizontal"):
     result = []
-    for key, val in dictvalue.iteritems():
-        result.append("<dt>{0}</dt><dd>{1}</dd>".format(key, val))
+    if isinstance(dictvalue, dict):
+        for key, val in dictvalue.iteritems():
+            result.append("<dt>{0}</dt><dd>{1}</dd>".format(key, val))
     if len(result):
         result.insert(0, '<dl class="{0}">'.format(css_class))
         result.append("</dl>")
