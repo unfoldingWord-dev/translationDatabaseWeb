151d129a056e610eab472952a5de75a5b30e3d4a
swilcox <steven@wilcoxzone.com>
2015-05-19 21:16:52 -0500

    new publisher and title views and relationships

--- td/uw/tests/test_models.py
+++ td/uw/tests/test_models.py
@@ -1,5 +1,5 @@
 from django.test import TestCase
-from ..models import Resource, Media, Title, Language
+from ..models import Resource, Media, Title, Language, Publisher
 
 
 class MediaTestCase(TestCase):
@@ -14,10 +14,21 @@ class TitleTestCase(TestCase):
         self.assertEquals(str(title), "Test Title")
 
 
+class PublisherTestCase(TestCase):
+    def test_string_representation(self):
+        publisher = Publisher(name="Test Publisher")
+        self.assertEquals(str(publisher), "Test Publisher")
+
+
 class ResourceTestCase(TestCase):
     @classmethod
     def setUpClass(cls):
+        cls.publisher = Publisher(name="Test Publisher 1")
+        cls.publisher.save()
+        cls.publisher2 = Publisher(name="Test Publisher 2")
+        cls.publisher2.save()
         cls.title = Title(name="Test Title", slug="test-title")
+        cls.title.publisher = cls.publisher
         cls.title.save()
         cls.media = Media(name="Media Title", slug="test-media")
         cls.media.save()
@@ -30,3 +41,7 @@ class ResourceTestCase(TestCase):
         resource.medias.add(self.media)
         resource.save()
         self.assertEquals(str(resource), "Test Title in Z Language")
+        self.assertEquals(str(resource.the_publisher()), "Test Publisher 1")
+        resource.publisher = self.publisher2
+        resource.save()
+        self.assertEquals(str(resource.the_publisher()), "Test Publisher 2")
