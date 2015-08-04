82c013ea50f77984228f36246e7502a28ac3128f
Patrick Altman <paltman@gmail.com>
2015-08-04 13:53:58 -0500

    Updated static build process

---
+++ static/src/js/glmap.js
@@ -0,0 +1,71 @@
+var $ = require("jquery");
+var d3 = require("d3");
+var Datamap = require("datamaps/dist/datamaps.world");
+
+var m_width = $("#mapcontainer").width(),
+              m_ratio = .42,
+              m_height = m_width * m_ratio;
+
+module.exports = {
+    drawMap: function () {
+        // @@@ pull this url from a data- attribute so we can use url reverser and not hardcode the url
+        d3.json("/uw/country_map_data.json", function(error, map_data) {
+            var map = new Datamap({
+                element: document.getElementById('mapcontainer'),
+                responsive: true,
+                width: m_width,
+                height: m_height,
+                fills: map_data["fills"],
+                data: map_data["country_data"],
+                geographyConfig: {
+                    hideAntarctica: true,
+                    borderColor: "#202020",
+                    highlightOnHover: true,
+                    highlightBorderWidth: 2,
+                    highlightFillColor: 'rgba(128,128,128,0.5)',
+                    popupTemplate: function(geography, data) {
+                        if (data) {
+                            geography.properties.url = data.url;
+                            return '<div class="hoverinfo"><div class="flag flag-' +
+                                    data.country_code.toLowerCase() + '"> </div><strong>' +
+                                    geography.properties.name + '</strong><br/>' +
+                                    data.gateway_languages.join('<br/>') +
+                                    '</div>';
+                        }
+                        else
+                            return '<div class="hoverinfo"><strong>' + geography.properties.name + '</strong></div>';
+                    }
+                },
+                done: function(datamap) {
+                    datamap.svg.selectAll('.datamaps-subunit').on('click', function(geography) {
+                        var $this = d3.select(this);
+                        var previousAttributes = JSON.parse( $this.attr('data-previousAttributes') );
+                            for ( var attr in previousAttributes ) {
+                                $this.style(attr, previousAttributes[attr]);
+                            }
+                        window.location.href = geography.properties.url;
+                    });
+                    this.element.style.paddingBottom = "38%";
+                }
+            });
+
+            d3.select(window).on('resize', function() {
+                map.resize();
+            })
+        })
+    },
+    submitDownloadForm: function(output_format) {
+        // Get the d3js SVG element
+        var tmp = document.getElementById("mapcontainer");
+        var svg = tmp.getElementsByTagName("svg")[0];
+        // Extract the data as SVG text string
+        var svg_xml = (new XMLSerializer).serializeToString(svg);
+
+        // Submit the <FORM> to the server.
+        // The result will be a file to download.
+        var form = document.getElementById("svgform");
+        form['output_format'].value = output_format;
+        form['data'].value = svg_xml ;
+        form.submit();
+    }
+};
---
+++ static/src/js/main.js
@@ -0,0 +1,86 @@
+require("../less/site.less");
+
+window.jQuery = window.$ = require("jquery");
+
+require("bootstrap");
+require("imports?define=>false!bootstrap-daterangepicker");
+require("eldarion-ajax");
+require("select2");
+require("datatables");
+require("datatables-bootstrap3-plugin");
+
+var glmap = require("./glmap");
+
+
+(function ($) {
+    $('[data-toggle="tooltip"]').tooltip()
+    $('[data-toggle="popover"]').popover()
+    glmap.drawMap();
+    $(document).on("click", ".btn-export-map", function() {glmap.submitDownloadForm("pdf");});
+    $.fn.languageSelector = function (options) {
+        var settings = $.extend({}, options);
+        return this.each(function () {
+            var $input = $(this);
+            $input.select2({
+              placeholder: "Search for a language...",
+              minimumInputLength: 2,
+              ajax: {
+                url: $input.data("source-url"),
+                dataType: "json",
+                quietMillis: 250,
+                data: function (term, page) {
+                  return {q: term}
+                },
+                results: function (data, page) {
+                  console.log(data.results);
+                  return {results: data.results}
+                },
+                cache: true
+              },
+              initSelection: function (element, callback) {
+                data = {
+                  "pk": element.data("lang-pk"),
+                  "ln": element.data("lang-ln"),
+                  "lc": element.data("lang-lc"),
+                  "lr": element.data("lang-lr")
+                }
+                callback(data);
+              },
+              id: function (lang) { return lang.pk; },
+              formatResult: function (lang) {
+                return "<strong>" + lang.ln + "</strong> <code>" + lang.lc + "</code> [" + lang.lr + "]";
+              },
+              formatSelection: function (lang) {
+                return "<strong>" + lang.ln + "</strong> <code>" + lang.lc + "</code> [" + lang.lr + "]";
+              },
+              escapeMarkup: function (m) { return m; }
+            });
+        });
+    }
+})(jQuery);
+
+$(function () {
+    $("table[data-source]").each(function () {
+        var $el = $(this);
+        $el.DataTable({
+            serverSide: true,
+            ajax: $el.data("source"),
+            stateSave: true
+        });
+    });
+    $(".select2-multiple").select2();
+    $(".language-selector").languageSelector();
+    $(".date-picker").daterangepicker({
+      singleDatePicker: true
+    });
+    $(document).on("eldarion-ajax:success", function (evt, $el) {
+        if ($el.hasClass("navbar-form")) {
+            var $el = $("<div>")
+              .addClass("alert")
+              .addClass("alert-info")
+              .html("<strong>Invite was sent!</sent>");
+            $("body").prepend($el);
+            setTimeout(function(){ $el.remove(); }, 3000);
+        }
+    });
+});
