1269ae6690c6f6e261aada4c395818720e0cea51
Vicky Leong <vleong2332>
2015-08-20 15:39:42 -0400

    Added charter, event views and adjusted the url names

--- td/tracking/urls.py
+++ td/tracking/urls.py
@@ -3,8 +3,15 @@ from . import views
 
 urlpatterns = [
 	url(r'^$', views.index, name = 'index'),
-	url(r'^add_charter/$', views.add_charter, name = 'add_charter'),
-	url(r'^add_event/$', views.add_event, name = 'add_event'),
-	url(r'^add_charter/success/$', views.charter_added, name = 'charter_added'),
-	url(r'^add_event/success$', views.event_added, name = 'event_added'),
+	
+	# url(r'^charter/?P<proj_num>[0-9a-zA-Z]+/$', views.charter, name = 'charter'),
+	# url(r'^charter/add/$', views.charter_add, name = 'charter_add'),
+	# url(r'^charter/add/success/$', views.charter_added, name = 'charter_added'),
+	# url(r'^charter/add/fail/$', views.charter_added, name = 'charter_added'),
+
+	# url(r'^event/?P<proj_num>[0-9a-zA-Z]+/$', views.event, name = 'event'),
+	# url(r'^event/add/$', views.event_add, name = 'event_add'),
+	# url(r'^event/add/success/$', views.event_added, name = 'event_added'),
+	# url(r'^event/add/fail/$', views.event_added, name = 'event_added'),
+
 ]
--- td/tracking/views.py
+++ td/tracking/views.py
@@ -1,18 +1,50 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Charter, Event
 
+
+#dashboard
 def index(request):
-	return HttpResponse("Tracking Index %s" % request);
+	charter_list = Charter.objects.order_by('-proj_num')
+	event_list = Event.objects.order_by('-charter')
+	context = {
+		'charter_list': charter_list,
+		'event_list': event_list
+	}
 
-def add_charter(request):
+	return render(request, 'tracking/index.html', context);
+
+
+def charter(request, proj_num):
+	try:
+		charter = Charter.objects.get(proj_num = proj_num)
+		context = {'charter': charter}
+	except Charter.DoesNotExist:
+		raise Http404('Charter record not found')
+	return render(request, 'tracking/charter.html', context)
+
+
+def event(request, event_id):
+	try:
+		event = Charter.objects.get(id = event_id)
+		context = {'event': event}
+	except Event.DoesNotExist:
+		raise Http404('Event record not found')
+	return render(request, 'tracking/event.html', context)
+
+
+def charter_add(request):
 	return HttpResponse("Add a new project charter %s" % request)
 
-def add_event(request):
-	return HttpResponse("Add a new project event %s" % request)
 
 def charter_added(request):
 	return HttpResponse("Project charter is added %s" % request)
+
+
+def event_add(request):
+	return HttpResponse("Add a new project event %s" % request)
+
 
 def event_added(request):
 	return HttpResponse("Project event is added %s" % request)
