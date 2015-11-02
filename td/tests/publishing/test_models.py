4c6edd2572a66081d5d4869857bc9cecd2d4c873
Christian Dailey <cdailey@eldarion.com>
2015-11-02 10:12:46 -0500

    Bugfix: notify_requestor_approved task passing user.id instead of user.
    * Adds unit tests for notify_requestor_approved task
    * Adds unit test coverage for PublishRequest.publish

---
+++ td/tests/publishing/test_models.py
@@ -0,0 +1,44 @@
+from django.test import TestCase
+from django.contrib.auth.models import User
+
+from mock import patch
+
+from td.models import Language
+from td.publishing.models import (
+    OfficialResource, OfficialResourceType, PublishRequest)
+
+
+class PublishRequestTestCase(TestCase):
+
+    def setUp(self):
+        resource_type, _ = OfficialResourceType.objects.get_or_create(
+            short_name="obs",
+            long_name="OpenBibleStory"
+        )
+        language, _ = Language.objects.get_or_create(
+            code="en",
+            name="English"
+        )
+        source_language, _ = Language.objects.get_or_create(
+            code="arc",
+            name="Aramaic"
+        )
+        self.user, _ = User.objects.get_or_create(
+            first_name="Test",
+            last_name="RequestorUser",
+            username="test_requestor_user"
+        )
+        self.model, _ = PublishRequest.objects.get_or_create(
+            requestor="Test publish request",
+            resource_type=resource_type,
+            language=language,
+            checking_level=1,
+            source_text=source_language
+        )
+
+    @patch("td.publishing.models.OfficialResource.ingest")
+    def test_publish(self, mock_ingest):
+        resource = self.model.publish(by_user=self.user)
+        self.assertIsInstance(resource, OfficialResource)
+        self.assertEqual(resource.language.code, "en")
+        mock_ingest.assert_called_once_with()
---
+++ td/tests/publishing/test_tasks.py
@@ -0,0 +1,49 @@
+from django.test import TestCase
+from django.contrib.auth.models import User
+
+from mock import patch, Mock
+
+from td.models import Language
+from td.publishing.models import OfficialResourceType, PublishRequest
+from td.publishing.tasks import approve_publish_request
+
+
+class PublishTasksTestCase(TestCase):
+
+    def setUp(self):
+        resource_type, _ = OfficialResourceType.objects.get_or_create(
+            short_name="obs",
+            long_name="OpenBibleStory"
+        )
+        language, _ = Language.objects.get_or_create(
+            code="en",
+            name="English"
+        )
+        source_language, _ = Language.objects.get_or_create(
+            code="la",
+            name="Latin"
+        )
+        self.user, _ = User.objects.get_or_create(
+            first_name="Test",
+            last_name="RequestorUser",
+            username="test_requestor_user"
+        )
+        self.request, _ = PublishRequest.objects.get_or_create(
+            requestor="Test publish request",
+            resource_type=resource_type,
+            language=language,
+            checking_level=1,
+            source_text=source_language
+        )
+
+    @patch("td.publishing.tasks.PublishRequest.publish")
+    @patch("td.publishing.tasks.notify_requestor_approved")
+    def test_approve_publish_request(self, mock_task, mock_publish):
+        mock_publish.return_value = Mock(pk=self.user.id)
+        resource_id = approve_publish_request(
+            self.request.id,
+            self.user.id
+        )
+        self.assertEqual(resource_id, self.user.id)
+        mock_publish.assert_called_once_with(by_user=self.user)
+        mock_task.delay.assert_called_once_with(self.request.id)
