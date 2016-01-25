f1acab88da6fd0c05690834043e37e9493e877ea
vleong2332 <vleong2332@gmail.com>
2016-01-25 09:02:30 -0500

    - Use datepicker for completion date - Installed bootstrap-datepicker

--- td/gl_tracking/forms.py
+++ td/gl_tracking/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from td.gl_tracking.models import Progress
+from td.tracking.forms import determine_widget
 # from django.core.urlresolvers import reverse as urlReverse
 
 
@@ -9,7 +10,36 @@ class VariantSplitModalForm(forms.Form):
 
 class ProgressForm(forms.ModelForm):
 
+    # Overriden add custom initialize the form
+    def __init__(self, *args, **kwargs):
+        super(ProgressForm, self).__init__(*args, **kwargs)
+        print '\nSELF FIELDS', self.fields
+        self.fields["completion_date"] = forms.DateField(
+            widget=forms.DateInput(
+                attrs={
+                    "class": "datepicker",
+                    "data-provide": "datepicker",
+                    # "data-date-format": "",
+                    "data-autoclose": "true",
+                    "data-keyboard-navigation": "true",
+                }
+            ),
+            input_formats=[
+                "%Y-%m-%d",
+                "%y-%m-%d",
+                "%m-%d-%Y",
+                "%m-%d-%y",
+                "%Y/%m/%d",
+                "%y/%m/%d",
+                "%m/%d/%Y",
+                "%m/%d/%y",
+            ],
+        )
+
     class Meta:
         model = Progress
-        exclude = ['language', 'type', 'created_by', 'created_at', 'modified_by', 'modified_at']
-        widgets = {}
+        exclude = ['language', 'type', 'created_by', 'created_at', 'modified_by', 'modified_at', 'is_done']
+        widgets = {
+            "notes": forms.Textarea(attrs={"rows": "4"}),
+            "methods": forms.CheckboxSelectMultiple(),
+        }
