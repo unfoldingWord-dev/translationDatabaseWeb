5d56d6436a40908c8a7f94bf26a1979fa4cffeb4
vleong2332 <vleong2332@gmail.com>
2016-01-26 13:32:55 -0500

    wrote unit tests for Phase, Document, and DocumentCategory

---
+++ td/tests/gl_tracking/test_models.py
@@ -0,0 +1,65 @@
+from django.test import TestCase
+# from django.utils import timezone
+
+from td.gl_tracking.models import (
+    Phase,
+    DocumentCategory,
+    Document,
+    Progress,
+    Partner,
+    Method,
+    GLDirector,
+)
+# from td.models import Language
+
+
+class PhaseTestCase(TestCase):
+
+    def test_string_representation_no_name(self):
+        """
+        Phase should be represented by 'BT Phase' + its number if it's not
+           created with a name
+        """
+        object1 = Phase.objects.create(number="1")
+        self.assertEqual(object1.__str__(), "BT Phase 1")
+
+    def test_string_representation_with_name(self):
+        """
+        Phase should be represented by its name if it's created with one
+        """
+        name = "Test Phase"
+        object1 = Phase.objects.create(number="1", name=name)
+        self.assertEqual(object1.__str__(), name)
+
+
+class DocumentCategoryTestCase(TestCase):
+
+    def test_string_representation(self):
+        """
+        DocumentCategory should be represented by its name
+        """
+        phase = Phase.objects.create(number="1")
+        object = DocumentCategory.objects.create(
+            name="Test Category",
+            phase=phase
+        )
+        self.assertEqual(object.__str__(), "Test Category")
+
+
+class DocumentTestCase(TestCase):
+
+    def test_string_representation(self):
+        """
+        Document should be represented by its name
+        """
+        phase = Phase.objects.create(number="1")
+        doc_cat = DocumentCategory.objects.create(
+            name="Test Category",
+            phase=phase
+        )
+        object = Document.objects.create(
+            name="Test Document",
+            code="td",
+            category=doc_cat
+        )
+        self.assertEqual(object.__str__(), "Test Document")
