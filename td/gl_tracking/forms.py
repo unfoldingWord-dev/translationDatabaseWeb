697b65b4b867867fa004a6f218ec4f6600a54ca6
vleong2332 <vleong2332@gmail.com>
2016-02-02 14:17:35 -0500

    Amended minor things for PR #415
    - Fixed styling in gl_tracking/dashboard.html
    - Fixed styling in gl_tracking/forms.py
    - Removed 2-digit year as a valid input for ProgressForm completion_date because it can be ambiguous

--- td/gl_tracking/forms.py
+++ td/gl_tracking/forms.py
@@ -11,26 +11,20 @@ class ProgressForm(forms.ModelForm):
     # Overriden add custom initialize the form
     def __init__(self, *args, **kwargs):
         super(ProgressForm, self).__init__(*args, **kwargs)
-        print '\nSELF FIELDS', self.fields
         self.fields["completion_date"] = forms.DateField(
             widget=forms.DateInput(
                 attrs={
                     "class": "datepicker",
                     "data-provide": "datepicker",
-                    # "data-date-format": "",
                     "data-autoclose": "true",
                     "data-keyboard-navigation": "true",
                 }
             ),
             input_formats=[
                 "%Y-%m-%d",
-                "%y-%m-%d",
                 "%m-%d-%Y",
-                "%m-%d-%y",
                 "%Y/%m/%d",
-                "%y/%m/%d",
                 "%m/%d/%Y",
-                "%m/%d/%y",
             ],
             required=False,
         )
