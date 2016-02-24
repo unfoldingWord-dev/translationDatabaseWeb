3d963e2f4d4700dc412f3e1016cd7461eb58e873
vleong2332 <vleong2332@gmail.com>
2016-02-24 09:45:37 -0500

    Refactored with python csv library
    Added user-friendly feedback
    
    Removed unnecessary stdout

--- td/imports/management/commands/language_alt_names.py
+++ td/imports/management/commands/language_alt_names.py
@@ -1,12 +1,12 @@
 """
 
 USAGE:
-    python manage.py language_alt_names <username> < <file>.csv
+    python manage.py language_alt_names (username) < (file).csv
 
 """
 
 import sys
-import traceback
+import csv
 
 from django.core.management.base import BaseCommand, CommandError
 from django.contrib.auth.models import User
@@ -16,71 +16,60 @@ from ....models import Language, LanguageAltName
 
 class Command(BaseCommand):
     help = "Import language alternate names from CSV"
-    data = sys.stdin.read()
+    data = sys.stdin
     user = None
-    code = None
-    name = None
+    lang_not_exist = []
+    lang_alt_name_exist = 0
+    lang_alt_name_created = 0
 
     def add_arguments(self, parser):
         parser.add_argument('username', nargs='+', type=str)
 
-    @staticmethod
-    def split_endings(data):
-        """
-        Split string based on CRLF or LF ending
-        :param data: Raw strings that may contain line endings
-        :return: Array of strings
-        """
-        return data.split("\r\n") if "\r\n" in data else data.split("\n")
-
-    def process_header(self, r):
-        """
-        Get column location from header
-        :param r:
-        :return:
-        """
-        try:
-            self.code = r.index("code")
-            self.name = r.index("name")
-        except ValueError:
-            traceback.print_exc()
-            raise CommandError("Failed in identifying fields in columns")
-
-    def process_entry(self, r, i):
+    def process_alt_name(self, code, name):
         """
         Create LanguageAltName and link it to Language
-        :param r: An array of values representing a single row of entry in CSV
-        :param i: Row number of the current entry in CSV
+        :param code: ISO-639-3 code
+        :param name: Alternate name of a language
         :return: None
         """
-        try:
-            obj, created = LanguageAltName.objects.get_or_create(code=r[self.code], name=r[self.name].decode("utf-8"))
-            if created:
-                # NOTE: Filter instead of get because there's a possibility that multiple languages share the same
-                #    iso-639-3 code
-                languages = Language.objects.filter(iso_639_3=r[self.code])
-                for language in languages:
-                    language.alt_name = obj
-                    language.source = self.user
-                    language.save()
-                    # NOTE: Even though the obj is saved when it's created, this second manual save is what triggers the
-                    #    right update for language.alt_names
-                    obj.save()
-                if len(languages) <= 0:
-                    self.stdout.write(self.style.NOTICE("Language '%s' doesn't exist" % r[self.code]))
-            else:
-                self.stdout.write(self.style.WARNING("LanguageAltName with code '%s' and name '%s' already exists" %
-                                                     (r[self.code], r[self.name].decode("utf-8"))))
-        except IndexError:
-            self.stdout.write(self.style.WARNING("No code or name in this row. Skipping row %s" % i))
-            traceback.print_exc()
+        obj, created = LanguageAltName.objects.get_or_create(code=code, name=name.decode("utf-8"))
+        if created:
+            self.lang_alt_name_created += 1
+            # NOTE: Using filter instead of get because there's a possibility that multiple languages share the same
+            #    iso-639-3 code. For example: 'pt' and 'pt-br'.
+            languages = Language.objects.filter(iso_639_3=code)
+            for language in languages:
+                language.alt_name = obj
+                language.source = self.user
+                language.save()
+                # NOTE: Even though the obj is saved when it's created, this second manual save is what triggers the
+                #    desired update for language.alt_names. Check out handle_alt_name_save() in td/receivers.py
+                obj.save()
+            if not len(languages):
+                self.lang_not_exist.append(code)
+        else:
+            self.lang_alt_name_exist += 1
+
+    def print_summary(self):
+        """
+        Let user knows the result of import activities
+        :return: None
+        """
+        self.stdout.write(self.style.SUCCESS("======= Summary ======="))
+        if len(self.lang_not_exist):
+            self.stdout.write(self.style.NOTICE("The following languages do not exist: %s. LanguageAltName(s) created,"
+                                                "but no connection made to Language" % self.lang_not_exist))
+        if self.lang_alt_name_exist:
+            self.stdout.write(self.style.NOTICE("%s LanguageAltName(s) already exist") % self.lang_alt_name_exist)
+        self.stdout.write(self.style.SUCCESS("Import is done. %s LanguageAltName(s) created"
+                                             % self.lang_alt_name_created))
 
     def handle(self, *args, **options):
         try:
             self.user = User.objects.get(username=options["username"][0])
-            rows = self.split_endings(self.data)
-            for i in range(len(rows)):
-                r = rows[i].split(',')
-                self.process_header(r) if i == 0 else self.process_entry(r, i)
+            reader = csv.DictReader(self.data)
+            for row in reader:
+                self.process_alt_name(row["code"], row["name"])
+            self.print_summary()
         except User.DoesNotExist:
             raise CommandError("User with username '%s' doesn't exist" % options["username"][0])
