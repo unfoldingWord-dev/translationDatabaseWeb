a17eee34ac3c43d0c086c1a88c23c15e24a167d1
vleong2332 <vleong2332@gmail.com>
2016-05-26 09:24:06 -0400

    temp

--- td/urls.py
+++ td/urls.py
@@ -46,6 +46,7 @@ urlpatterns = [
     url(r"^tracking/", include("td.tracking.urls", namespace="tracking")),
     url(r"^gl/", include("td.gl_tracking.urls", namespace="gl")),
     url(r"^api/", include("td.api.urls", namespace="api")),
+    url(r"^comment/", include("td.commenting.urls", namespace="comment")),
     url(r"^comments/", include("django_comments.urls")),
 
     url(r"^data-sources/additional-languages/$", AdditionalLanguageListView.as_view(), name="ds_additional_languages"),
