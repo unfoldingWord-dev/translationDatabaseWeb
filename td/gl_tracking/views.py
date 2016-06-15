9f5c49764f8f5ea78330003b8f32e105609f258d
Vicky Leong <vleong2332@gmail.com>
2016-06-15 15:33:16 -0400

    Change GL tracker to make a GET call (#614)
    * Change GL tracker to make a GET call
    
    There's really no need to do a POST, that will touch CSRF stuff, for GL
    tracker PhaseView.
    
    * Remove unnecessary code for RegionDetailView

--- td/gl_tracking/views.py
+++ td/gl_tracking/views.py
@@ -23,16 +23,9 @@ class HomeView(LoginRequiredMixin, TemplateView):
 class PhaseView(LoginRequiredMixin, TemplateView):
     template_name = "gl_tracking/_phase_view.html"
 
-    def post(self, request, *args, **kwargs):
-        """
-        Overridden to allow POST and avoid error when doing so. POST will be the default method of calling this view.
-        """
-        context = self.get_context_data()
-        return self.render_to_response(context)
-
     def get_context_data(self, *args, **kwargs):
         context = super(PhaseView, self).get_context_data(**kwargs)
-        phase = self.request.POST["phase"]
+        phase = self.kwargs.get("phase")
         regions = map_gls(Language.objects.filter(gateway_flag=True, variant_of=None), phase)
         for key, region in regions.iteritems():
             region["regional_progress"] = get_regional_progress(region["gateway_languages"])
@@ -48,13 +41,6 @@ class PhaseView(LoginRequiredMixin, TemplateView):
 class RegionDetailView(LoginRequiredMixin, DetailView):
     model = WARegion
     template_name = "gl_tracking/_region_detail.html"
-
-    def post(self, request, *args, **kwargs):
-        """
-        Overridden to allow POST and avoid error when doing so. POST will be the default method of calling this view.
-        """
-        context = self.get_context_data()
-        return self.render_to_response(context)
 
     def get_context_data(self, *args, **kwargs):
         context = super(RegionDetailView, self).get_context_data(**kwargs)
